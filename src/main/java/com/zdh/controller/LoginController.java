885da0aca4e5bb8e8c33b87401aae88f4ae582d6
Takohubuki <932421804@qq.com>
2019-04-09 13:21:25 +0800

    2019-04-09 13:30

--- src/main/java/com/zdh/controller/LoginController.java
+++ src/main/java/com/zdh/controller/LoginController.java
@@ -1,14 +1,24 @@
 package com.zdh.controller;
 
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 @Controller
 public class LoginController {
     @RequestMapping("/login")
-    public String login(String username, String password){
-
-
+    public String login(String username, String password) throws IOException {
+        String resource = "mybatis.xml";
+        InputStream resourceAsStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        SqlSession sqlSession = sqlSessionFactoryBuilder.openSession();
+        sqlSession.getMapper();
         return "success";
     }
 }
